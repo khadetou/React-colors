40b76e564f18b923f3a04b1cd27c68e4a65a4ead
khadetou <khadetou96@gmail.com>
2021-04-29 13:06:17 +0000

    palette component part

---
+++ src/components/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Palette = () => {
+    return (
+        <div className="Palette">
+            {/* Navbar */}
+                <div className="palette-colors">
+
+                </div>
+            {/* Footer */}
+        </div>
+    )
+}
+
+export default Palette
