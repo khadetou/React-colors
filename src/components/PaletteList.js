3ede7fb0adf50a81e51eeb805797d2f309bb4e88
khadetou <khadetou96@gmail.com>
2021-05-01 18:13:37 +0000

    mini palette

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import MiniPallete from './branches/MiniPalette';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyle= makeStyles({
+    root:{
+        backgroundColor: "blue",
+        height: "100vh",
+        display :"flex",
+        alignItems: "flex-start",
+        justifyContent: "center",
+    },
+
+    container:{
+            width: "50%",
+            display: "flex",
+            alignItems: "flex-start",
+            flexDirection: "column",
+            flexWrap: "wrap"
+    },
+
+    nav:{
+        color: "#fff",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    palets:{
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+});
 const PaletteList = ({palettes}) => {
 
+    const classes = useStyle();
 
     return (
-        <div>
-           {palettes.map(palette=>(
-               <Link to={`/palette/${palette.id}`} key={palette.id}>{palette.paletteName}</Link>
-           ))}
+        <div className={classes.root}>
+            <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React colors</h1>
+                    </nav>
+
+                    <div className={classes.palets}>
+                        {palettes.map(palette=>(
+                            <MiniPallete {...palette} key={palette.id}/>
+                        ))}
+                    </div>
+
+            </div>
         </div>
     )
 }
